f44bc21987dda7ae9ab72c5ae087c057647e2653
Antonio Calderón <antoniocalderon1028@gmail.com>
2026-09-23 18:28:41 +0000

    Streamlit

--- data1.py
+++ data1.py
@@ -3,5 +3,5 @@ import pandas as pd
 names_link = "https://raw.githubusercontent.com/softbytavz/FC26dataset/refs/heads/main/FC26_Data.csv"
 names_data = pd.read_csv(names_link)
 
-st.tittle ("FC26_Streamlit")
+st.title ("FC26_Streamlit")
 st.dataframe (names_data)
